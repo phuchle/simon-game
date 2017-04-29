ea215b75ff104128e77d162a9bc7b3eeef015251
Phuc Le <le.phuc.h@gmail.com>
2017-04-29 15:24:19 -0700

    Add new functions for how the game buttons will work.

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,77 +1,136 @@
 let pattern = [];
 let userPattern = [];
 
+const colors = ['green', 'red', 'yellow', 'blue'];
+const sounds = {
+  green: new Audio('sounds/simonSound1.mp3'),
+  red: new Audio('sounds/simonSound2.mp3'),
+  yellow: new Audio('sounds/simonSound3.mp3'),
+  blue: new Audio('sounds/simonSound4.mp3')
+};
+
+const click = {
+  green: function (target) {
+    target.style.backgroundColor = '#a7ffa7';
+    sounds.green.play();
+    setTimeout( ()=> {
+      target.style.backgroundColor = '#41ff41';
+    }, 500);
+  },
+  red: function (target) {
+    sounds.red.play();
+    target.style.backgroundColor = '#ff7676';
+    setTimeout( ()=> {
+      target.style.backgroundColor = 'red';
+    }, 500);
+  },
+  yellow: function (target) {
+    target.style.backgroundColor = '#fff9c4';
+    sounds.yellow.play();
+    setTimeout( ()=> {
+      target.style.backgroundColor = '#ffe400';
+    }, 500);
+  },
+  blue: function (target) {
+    target.style.backgroundColor = '#88b5ff';
+    sounds.blue.play();
+    setTimeout( ()=> {
+      target.style.backgroundColor = '#0079ff';
+    }, 500);
+  }
+};
+
 function startGameplay() {
   // randomly picks a color and adds it the pattern
   // plays the pattern
   // waits for user input
   // when lengths are the same, compares patterns
   // if correct, loop back
-  const colors = ['green', 'red', 'yellow', 'blue'];
+  activateGameButtons();
 
-  addColorButtonListeners();
+  let nextColor = colors[Math.floor(Math.random() * colors.length)];
+  pattern.push(nextColor);
+  setPlayThroughInterval();
+}
+
+function setPlayThroughInterval() {
+  let playThroughCount = 0;
+
+  let playThrough = window.setInterval( () => {
+    let targetColor = pattern[playThroughCount];
+    var targetButton = document.getElementById(targetColor);
+    click[targetColor](targetButton);
+
+    playThroughCount++;
+
+    if (playThroughCount === pattern.length) {
+      window.clearInterval(playThrough);
+    }
+  }, 1000);
 
 }
 
-function addColorButtonListeners() {
+function activateGameButtons() {
+  addGameButtonLights();
   addGreenListeners();
   addRedListeners();
   addYellowListeners();
   addBlueListeners();
 }
 
+function addGameButtonLights() {
+  colors.forEach(color => {
+    let docBody = document.body;
+    let targetButton = document.getElementById(color);
+
+    if (docBody.classList) { // modern browser
+      targetButton.classList.add('clickable');
+    } else { // IE8 support
+      targetButton.className += 'clickable';
+    }
+  });
+}
+
+function removeGameButtonLights() {
+  colors.forEach(color => {
+    let targetButton = document.getElementById(color);
+
+    targetButton.className = '';
+  });
+}
+
 function addGreenListeners() {
   let green = document.getElementById('green');
-  let greenSound = new Audio('sounds/simonSound1.mp3');
 
-  green.addEventListener('mousedown', () => {
+  green.addEventListener('click', () => {
     userPattern.push(green.id);
-    green.style.backgroundColor = '#a7ffa7';
-    greenSound.play();
-  });
-  green.addEventListener('mouseup', () => {
-    green.style.backgroundColor = '#41ff41';
+    click.green(green);
   });
 }
 
 function addRedListeners() {
   let red = document.getElementById('red');
-  let redSound = new Audio('sounds/simonSound2.mp3');
 
-  red.addEventListener('mousedown', () => {
+  red.addEventListener('click', () => {
     userPattern.push(red.id);
-    red.style.backgroundColor = '#ff7676';
-    redSound.play();
-  });
-  red.addEventListener('mouseup', () => {
-    red.style.backgroundColor = 'red';
+    click.red(red);
   });
 }
 
  function addYellowListeners() {
   let yellow = document.getElementById('yellow');
-  let yellowSound = new Audio('sounds/simonSound3.mp3');
 
-  yellow.addEventListener('mousedown', () => {
+  yellow.addEventListener('click', () => {
     userPattern.push(yellow.id);
-    yellow.style.backgroundColor = '#fff9c4';
-    yellowSound.play();
-  });
-  yellow.addEventListener('mouseup', () => {
-    yellow.style.backgroundColor = '#ffe400';
+    click.yellow(yellow);
   });
 }
 
 function addBlueListeners() {
   let blue = document.getElementById('blue');
-  let blueSound = new Audio('sounds/simonSound4.mp3');
 
-  blue.addEventListener('mousedown', () => {
+  blue.addEventListener('click', () => {
     userPattern.push(blue.id);
-    blue.style.backgroundColor = '#88b5ff';
-    blueSound.play();
-  });
-  blue.addEventListener('mouseup', () => {
-    blue.style.backgroundColor = '#0079ff';
+    click.blue(blue);
   });
 }
