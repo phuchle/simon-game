677a01f658d5e4338996b69e3667cdd0215069e0
Phuc Le <le.phuc.h@gmail.com>
2017-05-01 12:47:39 -0700

    Add preliminary functions for waiting for user input and checking pattern.  There is currently an infinite loop in checkUserPattern.

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,12 +1,14 @@
 let pattern = [];
 let userPattern = [];
+let waitForUserInput;
 
 const colors = ['green', 'red', 'yellow', 'blue'];
 const sounds = {
   green: new Audio('sounds/simonSound1.mp3'),
   red: new Audio('sounds/simonSound2.mp3'),
   yellow: new Audio('sounds/simonSound3.mp3'),
-  blue: new Audio('sounds/simonSound4.mp3')
+  blue: new Audio('sounds/simonSound4.mp3'),
+  wrongAnswer: new Audio('sounds/wrongAnswer.mp3')
 };
 
 const click = {
@@ -47,7 +49,10 @@ function startGameplay() {
   // when lengths are the same, compares patterns
   // if correct, loop back
   activateGameButtons();
+  makePattern();
+}
 
+function makePattern() {
   let nextColor = colors[Math.floor(Math.random() * colors.length)];
   pattern.push(nextColor);
   setPlayThroughInterval();
@@ -105,6 +110,7 @@ function addGreenListeners() {
   green.addEventListener('click', () => {
     userPattern.push(green.id);
     click.green(green);
+    checkUserPattern();
   });
 }
 
@@ -114,15 +120,17 @@ function addRedListeners() {
   red.addEventListener('click', () => {
     userPattern.push(red.id);
     click.red(red);
+    checkUserPattern();
   });
 }
 
- function addYellowListeners() {
+function addYellowListeners() {
   let yellow = document.getElementById('yellow');
 
   yellow.addEventListener('click', () => {
     userPattern.push(yellow.id);
     click.yellow(yellow);
+    checkUserPattern();
   });
 }
 
@@ -132,5 +140,39 @@ function addBlueListeners() {
   blue.addEventListener('click', () => {
     userPattern.push(blue.id);
     click.blue(blue);
+    checkUserPattern();
   });
+}
+
+function checkUserPattern() {
+  if (userPattern.length !== pattern.length) {
+    waitForUserInput = window.setTimeout( () => {
+      debugger;
+      sounds.wrongAnswer.play()
+      window.clearInterval(waitForUserInput);
+      setPlayThroughInterval();
+      checkUserPattern();
+    }, 2000);
+  } else {
+    window.clearInterval(waitForUserInput);
+    if (isCurrentUserPatternCorrect()) {
+      makePattern();
+      setPlayThroughInterval();
+      checkUserPattern();
+    }
+  }
+
+
+}
+
+function isCurrentUserPatternCorrect() {
+  let verdict = true;
+
+  for (let i = 0; i < userPattern.length; i++) {
+    if (userPattern[i] !== pattern[i]) {
+      verdict = false;
+    }
+  }
+  console.log(verdict);
+  return verdict;
 }
